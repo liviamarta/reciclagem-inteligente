0cb1adf1f7b9b93e434f79cd70e473f7c26b8125
Livia Marta <seuemail@exemplo.com>
2025-05-16 12:03:51 -0300

    Ajusta porta e adiciona gunicorn

--- app.py
+++ app.py
@@ -79,5 +79,8 @@ def ranking():
     return render_template('ranking.html', ranking_list=ranking_list)
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa a porta fornecida pelo Render
+    app.run(host='0.0.0.0', port=port)
